84e6383568b4019bb108f25b2c691358d7a2b31b
wa545464 <1982414933@qq.com>
2023-07-03 21:45:16 +0800

    ✨ feat: 补充插件和调整项目目录

---
+++ src/components/Menu/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="menuBox">菜单</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+.menuBox {
+    width: 224px;
+    height: 100%;
+    background-color: #171718;
+    color: #e2e8f0;
+}
+</style>
