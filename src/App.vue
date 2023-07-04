2bdff4d338d8ff7a079785d4796b7a843955f5e3
hickey <1982414933@qq.com>
2023-07-04 17:47:35 +0800

    feat: fixbug和接入prettier

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script setup lang="ts">
 import Menu from './components/Menu/index.vue';
-function a():number {
+function a(): number {
     const t = 1;
     return t;
 }
@@ -15,7 +15,7 @@ a();
 
 <style scoped lang="scss">
 .pageWrapper {
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 }
 </style>
