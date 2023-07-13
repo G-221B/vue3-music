0d5d2e9bfbec07b363344580927f6af46fa7306e
wa545464 <1982414933@qq.com>
2023-07-13 22:55:11 +0800

    feat: 完善左侧菜单

--- src/components/Menu/index.vue
+++ src/components/Menu/index.vue
@@ -1,19 +1,17 @@
 <template>
     <div class="menuBox">
-        <div v-for="menu in menuList" :key="menu.name">
+        <div v-for="menu in menuList" :key="menu.name" class="menuCategory">
             <div class="title">{{ menu.name }}</div>
             <div v-for="child in menu.childs" :key="child.name">
-                <div class="item">
+                <RouterLink :to="child.key" class="item">
                     <SvgIcon
                         :name="child.iconName"
                         width="18px"
                         height="18px"
                         color="red"
                     ></SvgIcon>
-                    <RouterLink :to="child.key" class="name">{{
-                        child.name
-                    }}</RouterLink>
-                </div>
+                    <span class="name">{{ child.name }}</span>
+                </RouterLink>
             </div>
         </div>
     </div>
@@ -35,13 +33,35 @@ const menuList = reactive(MenuList);
     .title {
         padding: 8px 16px;
         font-size: 12px;
+        margin-bottom: 8px;
+    }
+    .menuCategory {
         margin-bottom: 40px;
     }
-    .name {
+    .item {
+        display: flex;
+        width: 144px;
         padding: 6px 16px;
-        font-size: 14px;
+        align-items: center;
+        border-radius: 4px;
+        margin-top: 6px;
         text-decoration: none;
         color: #e2e8f0;
+        &:hover {
+            cursor: pointer;
+            background-color: #44403c;
+        }
+        &.router-link-active {
+            background-image: linear-gradient(
+                to right,
+                rgb(45, 212, 191),
+                rgb(52, 211, 153)
+            );
+        }
+        .name {
+            margin-left: 4px;
+            font-size: 14px;
+        }
     }
 }
 </style>
